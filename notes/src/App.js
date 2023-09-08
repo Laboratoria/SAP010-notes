0e646b86b1044312cca999b7a3390a7a2a9dd686
amandascam03 <amandascam03@gmail.com>
2023-09-08 19:16:57 -0300

    iniciando template da pagina inicial

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,13 +1,28 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Olá
-        </p>
-      </header>
-    </div>
-  );
+function FormLogin() {
+    return (
+        <form className="formulario">
+            <h2>LOGIN</h2>
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="senha" />
+            <button>LOGIN</button>
+            <p>ou</p>
+            <button>GOOGLE</button>
+            <p>
+                Ainda não tem uma conta? <a href="https">Cadastre-se</a>
+            </p>
+        </form>
+    );
 }
 
-export default App;
+
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Lab Notes</h1>
+                <p>Tome suas notas de uma forma simples e objetiva</p>
+            </header>
+            <FormLogin />
+        </div>
+    );
+}
